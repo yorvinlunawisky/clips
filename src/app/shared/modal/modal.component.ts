bff44ea743cd8ec6d59cf8b5d8c7df9207036c38
yorvinlunawisky <yorvin.luna@micm.gob.do>
2022-05-20 16:01:17 -0400

    Some divs added

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+  // providers: [ModalService]
+})
+export class ModalComponent implements OnInit {
+
+  constructor(public modal: ModalService) {
+   }
+
+  ngOnInit(): void {
+  }
+
+  closeModal() {
+    this.modal.toggleModal();
+  }
+
+}
