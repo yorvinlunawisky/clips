e4c657ad6e4cf2d4f59de80d3517ae2fc19de21a
yorvinlunawisky <yorvin.luna@micm.gob.do>
2022-05-25 12:05:12 -0400

    Added validators to inputs.

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -8,12 +8,25 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
   name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ])
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ])
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(18),
+    Validators.max(120) 
+  ])
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ])
+  confirm_password = new FormControl('', [
     Validators.required
   ])
-  email = new FormControl('')
-  age = new FormControl('')
-  password = new FormControl('')
-  confirm_password = new FormControl('')
   phoneNumber = new FormControl('')
 
 
