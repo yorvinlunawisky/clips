2c3eb71425164cd403f53eff362b5dd0cd98a8ca
yorvinlunawisky <yorvin0506@gmail.com>
2022-06-24 15:52:58 -0400

    Validating unique email.

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+import { RegisterValidators } from '../validators/register-validators';
 
 @Component({
   selector: 'app-register',
@@ -53,8 +54,7 @@ export class RegisterComponent {
     password: this.password,
     confirm_password: this.confirm_password,
     phoneNumber: this.phoneNumber
-
-  })
+  }, [RegisterValidators.match('password', 'confirm_password')])
 
   async register() {
    this.showAlert = true;
